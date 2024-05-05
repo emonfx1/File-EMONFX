c27e93c50114e86a4b2089301c7faf0521cae9f4
emonfx1 <153927593+emonfx1@users.noreply.github.com>
2024-05-05 15:23:46 +0600

    Add files via upload

---
+++ cbb.py
@@ -0,0 +1,28 @@
+#(©)Ordinary_Bots
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Emonfx7</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://telegra.ph/file/2e33375455678707d2c1d.mp4'>Click here</a>\n○ Channel : @Ordinary_Bots\n○ Support Admin : @Emonfx7</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
---
+++ route.py
@@ -0,0 +1,14 @@
+#(©)Ordinary_Bots
+#rymme
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Ordinary_Bots")
